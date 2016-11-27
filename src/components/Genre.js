abe42475d62b5eed97efced42eb86bbfed6d8a31
Ermilova Elena <ermilovaeo@gmail.com>
2016-11-28 00:58:24 +0700

    genre routing

--- src/components/Genre.js
+++ src/components/Genre.js
@@ -2,10 +2,16 @@ import React, { Component } from 'react'
 
 export default class Genre extends Component {
   render() {
-    return (
-      <div className='row'>
-        <div className='col-md-12'>Раздел /genre</div>
-      </div>
+    let template
+    if(this.props.params.release) {
+      template = <div className='col-md-12'>{this.props.children}</div>
+    } else {
+      template = <div className='col-md-12'>Здесь будет список релизов</div>
+    }
+    return (<div className='row'>
+              <h3 className='col-md-12'>{this.props.params.genre}</h3>
+              {template}
+            </div>
     )
   }
 }
